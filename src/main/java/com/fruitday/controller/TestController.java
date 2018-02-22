0a9d006020297db5d7c28c90483f69a2190c786b
王于晟 <wangys2@fruitday.com>
2018-02-22 09:41:46 +0800

    init

---
+++ src/main/java/com/fruitday/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.fruitday.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  * 测试controller
+ *   *
+ *    * @author liucheng
+ *     **/
+@RestController
+public class TestController {
+    @RequestMapping("/index")
+    public String method() {
+        return "hello world brande!";
+    }
+}
+
